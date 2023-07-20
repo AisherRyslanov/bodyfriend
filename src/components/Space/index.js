cdfd0cdc313a17ceb48346400acea8d91e05ddcb
AisherRyslanov <ryslanovalisher@gmail.com>
2023-07-20 14:50:44 +0600

    com

--- src/components/Space/index.js
+++ src/components/Space/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './style.scss'
 import bg from '../../img/Rovo_Medical_05.png'
-import video from "../../img/Phantom Medical Rovo_2.mp4";
 import alt from '../../img/Rovo_Medical_320_23.png'
 
 const Space = () => {
@@ -12,9 +11,9 @@ const Space = () => {
             <h1 className="space__listen">КОСМИЧЕСКОЕ ЗВУЧАНИЕ</h1>
             <h3 className="space__system">BODYDIFRIEND STEREO SYSTEM</h3>
             <p className="space__relax">Наслаждайтесь чистейшим стереозвучанием любимой музыки. Включита расслабляющую музыку для более глубокого погружения</p>
-            <video autoPlay muted loop controls className='space__video'>
-                <source src={video} type="video/mp4" />
-            </video>
+            {/*<video autoPlay muted loop controls className='space__video'>*/}
+            {/*    <source src={video} type="video/mp4" />*/}
+            {/*</video>*/}
         </div>
     );
 };
